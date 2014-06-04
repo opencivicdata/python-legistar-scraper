e8c9a7e18e59eaf6771bcd16e6fc60288e3a702e
twneale <twneale@gmail.com>
2014-06-04 16:36:28 -0400

    Ignore vendorized ansisterm and cli files

---
+++ assertions/country:us/state:ny/place:new_york/2014/events.py
@@ -0,0 +1,9 @@
+import datetime
+
+assertions = [
+
+ ({'end': datetime.datetime(2014, 8, 21, 14, 30),
+   'location': 'Council Chambers - City Hall'},
+  {'when': datetime.datetime(2014, 8, 21, 13, 30)}),
+
+]
