d10699b5be98fbf7a15069122861e4881b9089fb
twneale <twneale@gmail.com>
2014-06-17 14:01:20 -0400

    Add action scraping classes

--- legistar/bills.py
+++ legistar/bills.py
@@ -10,7 +10,7 @@ from legistar.views import SearchView, DetailView
 from legistar.fields import FieldAggregator, make_item, gen_items
 from legistar.fields import ElementAccessor
 from legistar.base import DictSetDefault, NoClobberDict
-
+from legistar.jurisdictions.utils import resolve_name
 # https://github.com/guelo/legistar-scrape/compare/fgregg:master...master
 
 class BillsFields(FieldAggregator):
@@ -110,23 +110,75 @@ class BillsDetailView(DetailView, BillsFields):
                     url=data.get_url(),
                     mimetype=mimetype)])
 
+    @make_item('actions', wrapwith=list)
+    def gen_action(self):
+        yield from self.Form(self)
+
 
 class BillsDetailTable(Table):
     sources_note = 'bill detail table'
 
 
-class BillsDetailTableRow(TableRow):
-
-    def _get_type(self):
-        typetext = self.get_field_text('type')
-        if typetext is not None:
-            typetext = typetext.lower()
-        return self.typetext_map.get(typetext, 'document')
-
-    @make_item('version')
-    def get_version(self):
-        return self.get_field_text('version')
-
-
 class BillsDetailForm(Form):
+    skip_first_submit = True
     sources_note = 'bill detail'
+
+
+class BillsDetailTableRow(TableRow, FieldAggregator, DateGetter):
+    sources_node = 'bill action table'
+
+    text_fields = (
+        ('action_by', 'organization'),
+        ('action', 'text'),
+        'version',
+        )
+
+    def get_detail_viewtype(self):
+        return BillsDetailAction
+
+    def get_detail_url(self):
+        return self.get_media_url('action_details')
+
+    @make_item('date')
+    def get_date(self):
+        return self._get_date('date')
+
+
+class ActionBase(FieldAggregator):
+
+    def get_prefix(self):
+        '''The settings prefix for this view.
+        '''
+        return 'BILL_ACTION'
+
+
+class BillsDetailAction(DetailView, ActionBase):
+    sources_note = 'bill action detail'
+
+    text_fields = (
+        'file_number', 'type', 'title', 'mover', 'seconder',
+        'result', 'agenda_note', 'minutes_note', 'action',
+        'action_text')
+
+    @make_item('votes', wrapwith=list)
+    def gen_votes(self):
+        table_path = self.get_config_value('table_class')
+        Table = resolve_name(table_path)
+        yield from self.make_child(Table, self)
+
+
+class BillsDetailActionTable(Table, ActionBase):
+    sources_note = 'bill action detail table'
+
+    def get_table_cell_type(self):
+        path = self.get_config_value('tablecell_class')
+        return resolve_name(path)
+
+    def get_table_row_type(self):
+        path = self.get_config_value('tablerow_class')
+        return resolve_name(path)
+
+
+class BillsDetailActionTableRow(TableRow, ActionBase):
+    sources_node = 'bill action detail table'
+    text_fields = ('person', 'vote')
