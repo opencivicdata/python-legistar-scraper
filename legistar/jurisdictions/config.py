9571a4cf8a1bad71eae597e924e1122c6118258b
twneale <twneale@gmail.com>
2014-07-21 16:51:36 -0400

    Basic Madison config additions

--- legistar/jurisdictions/config.py
+++ legistar/jurisdictions/config.py
@@ -191,10 +191,48 @@ class Madison(Config):
     '''XXX: Something horribly wrong with people paginated results.
     Keep getting page 1 back.
     '''
-    nicknames = ['madison']
-    division_id = 'ocd-division/country:us/state:wi/place:madison'
     root_url = 'http://madison.legistar.com/'
+    division_id = 'ocd-division/country:us/state:wi/place:madison'
+    nicknames = ['madison']
+    classification = 'government'
+    TIMEZONE = 'America/Chicago'
+
     PPL_SEARCH_TABLE_TEXT_FULLNAME = 'Name'
+    ORG_SEARCH_TABLE_TEXT_NAME = 'Boards, Commissions and Committees'
+    ORG_CLASSIFICATIONS = {
+        'ALLIED AREA TASK FORCE': 'commission',
+        'TRANSPORT 2020 IMPLEMENTATION TASK FORCE': 'commission',
+        'COMMON COUNCIL': 'legislature',
+        'COMMON COUNCIL - DISCUSSION': 'commission',
+        'COMMUNITY ACTION COALITION FOR SOUTH CENTRAL WISCONSIN INC': 'commission',
+        'COMMUNITY DEVELOPMENT AUTHORITY': 'commission',
+        'MADISON COMMUNITY FOUNDATION': 'commission',
+        'MADISON FOOD POLICY COUNCIL': 'commission',
+        'MADISON HOUSING AUTHORITY': 'commission',
+        'PARKING COUNCIL FOR PEOPLE WITH DISABILITIES': 'commission',
+    }
+    @make_item('person.district')
+    def person_district(self, data):
+        '''This corresponds to the label field on organizations posts.
+        '''
+        # First try to get it from bio.
+        dist = re.findall(r'District\s+\d+', data['notes'])
+        if dist:
+            return dist.pop()
+
+        # Then try website.
+        dist = re.findall(r'/district(\d+)/', data['website'])
+        if dist:
+            return dist.pop()
+
+        # Then email.
+        dist = re.findall(r'district(\d+)', data['email'])
+        if dist:
+            return dist.pop()
+
+    @overrides('OrgsAdapter.get_classification')
+    def orgs_get_classn(self):
+        return self.cfg.get_org_classification(self.data['name'])
 
 
 class JonesBoro(Config):
