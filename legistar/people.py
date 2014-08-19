f4ca8ccf9b8ad17ed8805b6a8f979e86aa34ede3
James Turk <james.p.turk@gmail.com>
2014-08-19 00:43:46 -0400

    cleaned up a bit

--- legistar/people.py
+++ legistar/people.py
@@ -8,7 +8,12 @@ from pupa.scrape import Scraper, Person
 
 class LegistarScraper(Scraper):
 
+    RESULTS_TABLE_XPATH = '//table[contains(@class, "rgMaster")]'
+    NO_RECORDS_FOUND_TEXT = ['No records were found', 'No records to display.']
+    BAD_QUERY_TEXT = ['Please enter your search criteria.']
+
     def lxmlopen(self, url, payload=None):
+        """ use scrapelib to open a file and get a normalized lxml.html doc from it """
         if payload:
             page = self.post(url, data=payload)
         else:
@@ -18,6 +23,7 @@ class LegistarScraper(Scraper):
         return doc
 
     def get_next_page(self, doc):
+        """ mimic the __doPostBack js """
         payload = {}
         payload['__EVENTARGUMENT'] = ''
         payload['__VIEWSTATE'] = doc.xpath("//input[@name='__VIEWSTATE']/@value")[0]
@@ -30,18 +36,20 @@ class LegistarScraper(Scraper):
             return self.lxmlopen(url, payload)
 
     def parse_search_page(self, doc):
-        RESULTS_TABLE_XPATH = '//table[contains(@class, "rgMaster")]'
-        NO_RECORDS_FOUND_TEXT = ['No records were found', 'No records to display.']
-        BAD_QUERY_TEXT = ['Please enter your search criteria.']
+        """
+            get all result rows from search and yield them out as lists of tuples
+            [(header, value), (header2, value2), etc.]
 
-        tbl = doc.xpath(RESULTS_TABLE_XPATH)[0]
+            transparently takes care of pagination within the function
+        """
+        tbl = doc.xpath(self.RESULTS_TABLE_XPATH)[0]
 
         headers = [th.text_content().replace('\xa0', ' ').strip()
                    for th in tbl.xpath('.//th[contains(@class, "rgHeader")]')]
 
-        # get data
         for tr in tbl.xpath('.//tr')[1:]:
-            # Skip the pagination rows.
+
+            # Skip the pagination & non-data rows.
             class_attr = tr.attrib.get('class', '')
             if ('rgPager' in class_attr or
                 'rgFilterRow' in class_attr or
@@ -50,41 +58,43 @@ class LegistarScraper(Scraper):
                ):
                 continue
 
-            for no_records_text in NO_RECORDS_FOUND_TEXT:
+            for no_records_text in self.NO_RECORDS_FOUND_TEXT:
                 if no_records_text in tr.text_content().strip():
-                    msg = 'No records found in %r. Moving on.'
-                    self.debug(msg % self)
+                    self.debug('No records found, moving on.')
                     raise StopIteration()
 
-            for bad_query_text in BAD_QUERY_TEXT:
+            for bad_query_text in self.BAD_QUERY_TEXT:
                 if bad_query_text in tr.text_content().strip():
-                    msg = ('Invalid query! This means the function that '
-                           'determines the query data probably needs edits.')
-                    self.critical(msg)
+                    self.critical('Invalid query!')
                     raise StopIteration()
 
             # get cells & wrap them up with the headers
             cells = tr.xpath('.//td')
             assert len(cells) == len(headers)
-
             yield list(zip(headers, cells))
 
+        # after rows are depleted, check if there is another page and recurse
         next_page = self.get_next_page(doc)
         if next_page:
             yield from self.parse_search_page(next_page)
 
-
     def parse_search_row(self, row):
+        """
+            use self.SEARCH_ROW_MAPPING to convert <th> text to expected variable names 
+
+            instead of overriding this function you can modify SEARCH_ROW_MAPPING
+        """
         result = {}
         for name, td in row:
-            # TODO: make sure mapping is complete
             result[self.SEARCH_ROW_MAPPING[name]] = td.text_content().strip()
         return result
 
-    def get_detail_url(self, row):
-        return row[0][1].xpath('.//a/@href')[0]
-
     def parse_detail_page(self, url, item):
+        """
+            pull the rows out of the tblMain detail page
+
+            instead of overriding this function you can modify DETAIL_PAGE_MAPPING
+        """
         doc = self.lxmlopen(url)
         tbl = doc.xpath('//table[@id="ctl00_ContentPlaceHolder1_tblMain"]')[0]
         for tr in tbl.xpath('.//tr'):
@@ -94,33 +104,48 @@ class LegistarScraper(Scraper):
                 self.warning('only one <td> for ' + label)
                 continue
             content = tds[1].text_content().strip()
-            # TODO: make sure mapping is complete
             item[self.DETAIL_PAGE_MAPPING[label]] = content
-
-        item['sources'] = [url]
 
         return self.obj_from_dict(item)
 
+    def get_detail_url(self, row):
+        """
+            get the URL to the row's detail page
+            (overriding is recommended if the usual pattern doesn't hold)
+        """
+        return row[0][1].xpath('.//a/@href')[0]
+
     def obj_from_dict(self, item):
+        """
+            implemented in each subclass and recommended hook for overriding the conversion
+            from collected data to a dict
+        """
         raise NotImplementedError('obj_from_dict needs to be implemented in a subclass')
 
     def skip_item(self, item):
+        """ can be overridden with custom logic to skip unruly items """
         return False
 
     def scrape(self):
-        self.user_agent = 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1'
-        
+        # seems to help w/ aspx stuff?
+        self.user_agent = ('Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 '
+                           'Fedora/3.0.1-1.fc9 Firefox/3.0.1')
+        # get first page
         url = urllib.parse.urljoin(self.jurisdiction.LEGISTAR_ROOT_URL, self.SEARCH_PAGE)
         doc = self.lxmlopen(url)
+
         for row in self.parse_search_page(doc):
             item = self.parse_search_row(row)
+            item['sources'] = [url]
             if not self.skip_item(item):
                 detail_url = self.get_detail_url(row)
+                item['sources'].append(detail_url)
                 yield self.parse_detail_page(detail_url, item)
 
 
 class LegistarPersonScraper(LegistarScraper):
 
+    #TABS = {'events': 'Calendar.aspx', 'orgs': 'Departments.aspx', 'bills': 'Legislation.aspx',}
     SEARCH_PAGE = 'People.aspx'
     SEARCH_ROW_MAPPING = {
         'Person Name': 'name',
@@ -195,5 +220,3 @@ class LegistarPersonScraper(LegistarScraper):
     PPL_DETAIL_TABLE_TEXT_START_DATE = 'Start Date'
     PPL_DETAIL_TABLE_TEXT_END_DATE = 'End Date'
     PPL_DETAIL_TABLE_TEXT_APPOINTED_BY = 'Appointed By'
-
-    #TABS = {'events': 'Calendar.aspx', 'orgs': 'Departments.aspx', 'bills': 'Legislation.aspx',}
