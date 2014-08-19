e719846f953db2a99f3ab044474cf9356b469144
James Turk <james.p.turk@gmail.com>
2014-08-19 00:03:18 -0400

    removing these tests (see extended comment)
    this might be a decent idea, but they were already failing and ignored
    if we are going to add scrape-wide tests (and we should)
    it should probably be at the pupa level not in a single scraper
    where it is easily ignored

--- assertions/country:us/state:ny/place:new_york/2014/events.py
+++
@@ -1,9 +0,0 @@
-import datetime
-
-assertions = [
-
- ({'end': datetime.datetime(2014, 8, 21, 14, 30),
-   'location': 'Council Chambers - City Hall'},
-  {'when': datetime.datetime(2014, 8, 21, 13, 30)}),
-
-]
