cea6adeef22a6e1ed29996c5ba2a5b52475ef775
Forest Gregg <fgregg@uchicago.edu>
2018-04-18 16:37:07 -0500

    break out fixture to conftest

---
+++ tests/conftest.py
@@ -0,0 +1,13 @@
+import pytest
+
+from legistar import base
+
+@pytest.fixture(scope="module")
+def scraper():
+    scraper = base.LegistarAPIScraper(None, None)
+    scraper.BASE_URL = 'http://webapi.legistar.com/v1/chicago'
+    scraper.retry_attempts = 0
+    scraper.requests_per_minute = 0
+    return scraper
+
+
--- tests/test_search.py
+++ tests/test_search.py
@@ -2,15 +2,6 @@ import pytest
 
 from legistar import base
 
-@pytest.fixture(scope="module")
-def scraper():
-    scraper = base.LegistarAPIScraper(None, None)
-    scraper.BASE_URL = 'http://webapi.legistar.com/v1/chicago'
-    scraper.retry_attempts = 0
-    scraper.requests_per_minute = 0
-    return scraper
-
-
 class TestAPISearch(object):
 
     def test_search_raises(self, scraper):
