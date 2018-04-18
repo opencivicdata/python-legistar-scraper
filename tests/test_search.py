3dc986c02b0b5c56c58fa8328e106c29bd68a5b9
Forest Gregg <fgregg@uchicago.edu>
2018-04-18 16:14:42 -0500

    use pytest conventions

--- tests/test_search.py
+++ tests/test_search.py
@@ -1,27 +1,27 @@
-import unittest
-
 import pytest
 
 from legistar import base
 
+@pytest.fixture(scope="module")
+def scraper():
+    scraper = base.LegistarAPIScraper(None, None)
+    scraper.BASE_URL = 'http://webapi.legistar.com/v1/chicago'
+    scraper.retry_attempts = 0
+    scraper.requests_per_minute = 0
+    return scraper
 
-class TestAPISearch(unittest.TestCase):
 
-    def setUp(self):
-        self.scraper = base.LegistarAPIScraper(None, None)
-        self.scraper.BASE_URL = 'http://webapi.legistar.com/v1/chicago'
-        self.scraper.retry_attempts = 0
-        self.scraper.requests_per_minute = 0
-        
-    def test_search_raises(self):
+class TestAPISearch(object):
+
+    def test_search_raises(self, scraper):
         with pytest.raises(ValueError):
-            results = self.scraper.search('/events/', 'EventId',
-                                          "MatterFile eq 'O2010-5046'")
+            results = scraper.search('/events/', 'EventId',
+                                     "MatterFile eq 'O2010-5046'")
             list(results)
             
-    def test_search(self):
-        results = self.scraper.search('/matters/', 'MatterId',
-                                      "MatterFile eq 'O2010-5046'")
+    def test_search(self, scraper):
+        results = scraper.search('/matters/', 'MatterId',
+                                 "MatterFile eq 'O2010-5046'")
         
         
         assert len(list(results)) == 1
