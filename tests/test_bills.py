5c55f1ea27b13266888cfc08900b03b00f8b1c94
Forest Gregg <fgregg@uchicago.edu>
2019-08-05 16:57:21 -0500

    tests for various problems that came up in big chicago scrape

--- tests/test_bills.py
+++ tests/test_bills.py
@@ -3,7 +3,7 @@ import re
 import requests_mock
 
 
-def test_topics(api_bill_scraper, matter_index, all_indexes):
+def test_topics(metro_api_bill_scraper, matter_index, all_indexes):
     with requests_mock.Mocker() as m:
         matter_matcher = re.compile(r'/matters/5036/indexes')
         m.get(matter_matcher, json=matter_index, status_code=200)
@@ -11,11 +11,28 @@ def test_topics(api_bill_scraper, matter_index, all_indexes):
         all_matcher = re.compile(r'/metro/indexes')
         m.get(all_matcher, json=all_indexes, status_code=200)
 
-        matter_topics = api_bill_scraper.topics(5036)
-        all_topics = list(api_bill_scraper.topics())
+        matter_topics = metro_api_bill_scraper.topics(5036)
+        all_topics = list(metro_api_bill_scraper.topics())
 
         # Assert only matter indexes are returned when matter ID passed
         assert len(matter_index) == len(matter_topics)
 
         # Assert all indexes returned when no matter ID passed
         assert len(all_indexes) == len(all_topics)
+
+
+def test_duplicate_events(chicago_api_bill_scraper, caplog):
+    chicago_api_bill_scraper.history('38768')
+    assert 'appears more than once' in caplog.text
+
+
+def test_no_duplicate(chicago_api_bill_scraper, caplog):
+    chicago_api_bill_scraper.history('38769')
+    assert 'appears more than once' not in caplog.text
+
+
+def test_404_votes(chicago_api_bill_scraper):
+    votes = chicago_api_bill_scraper.votes('408134')
+    assert votes == []
+
+    assert chicago_api_bill_scraper.votes('200942')
